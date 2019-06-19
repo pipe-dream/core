6af7851142391b70aae82c7c36db8ca8df4642fa
Anders Jurisoo <jurisoo@hotmail.com>
2019-06-19 12:32:56 +0200

    I can register components from CORE :D

--- src/PipeDream.js
+++ src/PipeDream.js
@@ -1,7 +1,26 @@
 import defaultStore from './resources/js/defaultStore/index.js'
 
-export default class PipeDream {
-    static defaultStore() {
-        return defaultStore
+class PipeDream {
+    constructor(options) {
+        this.fileFactory = options.fileFactory
+    }
+
+    get defaultStore() {
+        return {
+            state: {
+                ...defaultStore.state,
+                ...{
+                    availablePipes: this.fileFactory.pipes(),
+                    selectedPipes: this.fileFactory.pipes().map(pipe => pipe.name),
+                }
+            },
+            mutations: defaultStore.mutations,
+            actions: defaultStore.actions,
+            getters: defaultStore.getters,
+        }
     }
 }
+
+
+
+export default PipeDream
--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import PipeDream from './PipeDream'
+import AppHeader from './resources/js/components/AppHeader'
 
 export default PipeDream
+
+export const components = {
+    AppHeaderz: AppHeader
+}
