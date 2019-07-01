3e96145ece4b37651a07c404b8846276a47357b8
Anders Jurisoo <jurisoo@hotmail.com>
2019-07-01 10:31:30 +0200

    Implemented string load and tests

--- test/objectModel/SketchParser.test.js
+++ test/objectModel/SketchParser.test.js
@@ -1,12 +1,41 @@
 import SketchParser from 'objectModel/SketchParser';
+import sketch from './testSketches/sketchStringLoader'
+
+/* NOT NICE FIX - these constants are bootstrapped in the repos that use core
+* thus not available here :/ */
+window.___SINGLE_LINE_BREAK___ = "\n"
+window.___DOUBLE_LINE_BREAK___ = ___SINGLE_LINE_BREAK___*2
 
 describe("SketchParser", () => {
-
-    const EMPTY_SKETCH = '';
-
     test('an empty sketch should yield zero segments', () => {
         expect(
-            SketchParser.makeWithText(EMPTY_SKETCH).segment().length
+            SketchParser.parse(sketch.EMPTY).segment().length
         ).toBe(0);
+
+        /* even if the empty sketch is dirty */
+        expect(
+            SketchParser.parse(
+                sketch.MIXED_WHITESPACE_AND_COMMENTS
+            ).segment().length
+        ).toBe(0);        
+    });    
+
+    test('a single word should yield one segment', () => {
+        expect(
+            SketchParser.parse(sketch.SINGLE_MODEL).segment().length
+        ).toBe(1);
+
+        /* even if dirty */
+        let dirty_single_model = [
+            sketch.MIXED_WHITESPACE_AND_COMMENTS,
+            sketch.SINGLE_MODEL,
+            sketch.MIXED_WHITESPACE_AND_COMMENTS
+        ].join()
+
+        expect(
+            SketchParser.parse(
+                dirty_single_model
+            ).segment().length
+        ).toBe(1);        
     });
 });
---
+++ test/objectModel/testSketches/sketchStringLoader.js
@@ -0,0 +1,55 @@
+/* POLYFILL ********************************************************
+* LETS US USE require.context to load all strings
+*/
+
+// This condition actually should detect if it's an Node environment
+if (typeof require.context === 'undefined') {
+    const fs = require('fs');
+    const path = require('path');
+  
+    require.context = (base = '.', scanSubDirectories = false, regularExpression = /\.js$/) => {
+      const files = {};
+  
+      function readDirectory(directory) {
+        fs.readdirSync(directory).forEach((file) => {
+          const fullPath = path.resolve(directory, file);
+  
+          if (fs.statSync(fullPath).isDirectory()) {
+            if (scanSubDirectories) readDirectory(fullPath);
+  
+            return;
+          }
+  
+          if (!regularExpression.test(fullPath)) return;
+  
+          files[fullPath] = true;
+        });
+      }
+  
+      readDirectory(path.resolve(__dirname, base));
+  
+      function Module(file) {
+        return require(file);
+      }
+  
+      Module.keys = () => Object.keys(files);
+  
+      return Module;
+    };
+  }
+/* POLYFILL END *********************************************************/
+
+
+var context = require.context('./', true, /\.(string)$/);
+
+var strings = {};
+context.keys().forEach((filename)=>{
+  // take filename only
+  let fileKey = /[^/]*$/.exec(filename)[0]
+  // remove .string
+  fileKey = fileKey.replace(".string", "")
+  // attach to dictionary
+  strings[fileKey] = context(filename);
+});
+
+export default strings
