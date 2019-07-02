a01c2617464c381ef46af708ebaa4c089e6ebc6b
Anders Jurisoo <jurisoo@hotmail.com>
2019-07-02 16:09:27 +0200

    Added more segment tests

--- test/objectModel/Segment.test.js
+++ test/objectModel/Segment.test.js
@@ -1,5 +1,5 @@
 import Segment from 'objectModel/Segment'
-import sketch from './testSegments/segmentStringLoader'
+import segment from './testSegments/segmentStringLoader'
 
 /* NOT NICE FIX - these constants are bootstrapped in the repos that use core
 * thus not available here :/ */
@@ -9,9 +9,15 @@ window.___DOUBLE_LINE_BREAK___ = ___SINGLE_LINE_BREAK___.repeat(2)
 describe("Segment", () => {
     test('a segment cant be created from an empty string', () => {
         const attemptToCreateEmptySegment = () => {
-            new Segment("")
+            new Segment(segment.EMPTY)
         }
         
         expect(attemptToCreateEmptySegment).toThrow(TypeError)
-    });    
+    });
+    
+    test('a segment created from a single word has no attributes', () => {
+        expect(
+            new Segment(segment.MODEL_WITHOUT_ATTRIBUTES).attributes.length
+        ).toBe(0)
+    });
 });
