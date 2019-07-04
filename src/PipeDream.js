d32328c22c55d8aa8eae65763e9a45ebfb83a360
Anders Jurisoo <jurisoo@hotmail.com>
2019-07-04 06:51:31 +0200

    Cleanup

--- src/PipeDream.js
+++ src/PipeDream.js
@@ -1,8 +1,29 @@
-import defaultStore from './defaultStore/index.js'
+import defaultStore from './defaultStore/index.js';
 
 export default class PipeDream {
     constructor(options) {
         this.options = options
+    }
+
+    /* Idea on how to merge custom modules */
+    /* Usage? */
+    /* Use global prefix ~pd.Attribute */
+    /* Use store */
+    /* Use prefix when importing */
+    modules(overriddenModules) {
+
+        let defaultModules = {            
+            Attribute,
+            AttributeFactory,
+            ObjectModelCollection,
+            ObjectModelEntity,
+            ObjectModelEntityFactory,
+            Segment,
+            SegmentRow,
+            SketchParser
+        }
+
+        return merge(defaultModules, overriddenModules)
     }
 
     get defaultStore() {
--- src/objectModel/SketchParser.js
+++ src/objectModel/SketchParser.js
@@ -30,23 +30,6 @@ export default class SketchParser {
         return this;
     }
 
-    clean2() {
-        this.text = this.text
-            // remove comments
-            .replace(/\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*$/gm, "")        
-            // trim preciding line space
-            .replace(/[^\S\r\n]/gm,"")        
-            // trim trailing line space
-            .replace(/[\t]+$/gm,"")        
-            // trim preciding newlines
-            .replace(/^\n+/,"")
-            // trim trailing newlines
-            .replace(/\n+$/, "")
-            // remove exessive newlines
-            .replace(/\n\s+\n/, ___DOUBLE_LINE_BREAK___)
-        return this;
-    }    
-
     /* returns an array with items of type Segment */
     segment() {
         return !this.text ? [] : this.text.split(/\n\s*\n/).map((chunk) => Segment.fromText(chunk))
