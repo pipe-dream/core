e1e87631aa07a68fe94b191077aacd83801c2bc2
Anders Jurisoo <jurisoo@hotmail.com>
2019-07-16 11:46:09 +0200

    Test import Formatter instead of F

--- test/utilities/Formatter.test.js
+++ test/utilities/Formatter.test.js
@@ -1,16 +1,16 @@
-import F from 'utilities/formatter';
+import Formatter from 'utilities/Formatter';
 
 describe("Formatter", () => {
 
     const TEST_WORD = 'anne have three apple';
 
     test('it should snake case properly', () => {
-        expect(F.snakeCase(TEST_WORD))
+        expect(Formatter.snakeCase(TEST_WORD))
             .toBe('anne_have_three_apple');
     });
 
     test('it should camel case properly', () => {
-        expect(F.camelCase(TEST_WORD))
+        expect(Formatter.camelCase(TEST_WORD))
             .toBe('anneHaveThreeApple');
     });
 
