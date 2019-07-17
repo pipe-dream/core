b994976f0e0b396d0779836081d3f29b099e0301
Anders Jurisoo <jurisoo@hotmail.com>
2019-07-17 18:14:47 +0200

    Added a dummy GUI

--- src/components/WorkspaceTabs/Design/StackTab.vue
+++ src/components/WorkspaceTabs/Design/StackTab.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="flex flex-col max-w-3xl mx-auto px-8 bg-white pt-4 text-sm">
         <h3>Select your stack</h3>
+        <div class="flex flex-col">        
+            <span><input type="checkbox" class="mr-2">Laravel</span>
+            <span><input type="checkbox" class="mr-2">Vue</span>
+        </div>
     </div>  
 </template>
 
