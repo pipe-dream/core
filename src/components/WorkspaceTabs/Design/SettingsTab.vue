df06e02788c1a92c46c9adc076338699dbd12dd7
Adhika Setya Pramudita <adhika.setyap@gmail.com>
2019-08-17 21:48:09 +0700

    Use title property instead of name
    Avoid weird pipe/factory name when the JS file is minified

--- src/components/WorkspaceTabs/Design/SettingsTab.vue
+++ src/components/WorkspaceTabs/Design/SettingsTab.vue
@@ -1,22 +1,22 @@
 <template>
     <div class="flex flex-col max-w-3xl mx-auto px-8 bg-white pt-4 text-sm">
         <div v-for="fileFactory in fileFactories"
-            v-bind:key="fileFactory.name"
+            v-bind:key="fileFactory.title"
             class="border p-4"
         >
-            <h2>{{ fileFactory.name }}</h2>
+            <h2>{{ fileFactory.title }}</h2>
             <!-- FILE FACTORY PIPES -->
             <div class="flex flex-col ml-4 mt-4">
                 <h3 class="mb-2">Pipes</h3>
-                <div v-for="pipe in fileFactory.pipes()" v-bind:key="pipe.name" class="flex items-center">
-                    <input type="checkbox" :checked="isChecked(pipe.name)" @click="toggle(pipe.name)"> 
-                    <p class="ml-2">{{pipe.name}}</p>
+                <div v-for="pipe in fileFactory.pipes()" v-bind:key="pipe.title" class="flex items-center">
+                    <input type="checkbox" :checked="isChecked(pipe.title)" @click="toggle(pipe.title)">
+                    <p class="ml-2">{{pipe.title}}</p>
                 </div>
             </div>
 
             <!-- FILE FACTORY SETTINGS -->
-            <div class="flex flex-col ml-4 mt-4" v-for="setting in fileFactory.settings()" v-bind:key="setting.name">                
-                    <h3 class="mb-2">{{setting.name}}</h3>
+            <div class="flex flex-col ml-4 mt-4" v-for="setting in fileFactory.settings()" v-bind:key="setting.title">
+                    <h3 class="mb-2">{{setting.title}}</h3>
                     <input type="text" name="fname" class="border" :value="setting.default"> 
             </div>
 
