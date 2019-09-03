84ee40fd57d915a1ee69aee9e4abb4f2ea86811a
Anders Jurisoo <jurisoo@hotmail.com>
2019-09-03 14:59:19 +0200

    Stack tab design

--- src/components/WorkspaceTabs/Design/StackTab.vue
+++ src/components/WorkspaceTabs/Design/StackTab.vue
@@ -1,24 +1,33 @@
 <template>
-    <div class="flex flex-col max-w-lg mx-auto px-8 bg-white pt-4 text-sm mt-4">
-        <p class="font-semibold text-gray-800 text-base mb-2">Primary file factory</p>
-        <div class="flex flex py-1">
-            <select v-model="masterFileFactory" class="flex-1 bg-white border">
+    <div class="flex flex-col max-w-lg mx-auto px-8 bg-white pt-4 text-sm mt-4 border border-gray-200 pb-12">
+        <p class="font-semibold text-gray-800 text-base mb-2">Primary tech</p>
+        <div class="flex flex-col bg-gray-100 p-4 border-l-4 border-blue-400">
+            <select v-model="masterFileFactory" class="bg-white border font-semibold">
                 <option v-for="fileFactory in fileFactories" v-bind:key="fileFactory.title"
                     :value="fileFactory"
                 >
                     {{fileFactory.title}}
                 </option>
             </select>
+            <p class="text-gray-800 text-xs px-2 mt-2">
+            The primary file factory determines the behavior of the Pipe Dream workbench. Typically use a back end framework as primary file factory.
+            </p>            
         </div>           
 
 
-        <p class="font-semibold text-gray-800 text-base mt-2 mb-2">Secondary file factories</p>
-        <div v-for="secondary in secondary" v-bind:key="secondary.title" class="flex items-center py-1 mb-4">
-            <input type="checkbox" :name="secondary.title" :value="true" @change="toggleSecondaryFileFactory"
-                :checked="enabledFileFactories.includes(secondary.title)"
-            >
-            <p class="ml-2">{{secondary.title}}</p>    
-        </div>                          
+        <p class="font-semibold text-gray-800 text-base mt-8 mb-2">Secondary tech</p>
+        <div class="flex flex-col bg-gray-100 p-4">
+            <div v-for="secondary in secondary" v-bind:key="secondary.title" class="flex items-center py-1 mb-4">
+                <input type="checkbox" :name="secondary.title" :value="true" @change="toggleSecondaryFileFactory"
+                    :checked="enabledFileFactories.includes(secondary.title)"
+                >
+                <p class="ml-2 font-semibold text-sm">{{secondary.title}}</p>    
+            </div>
+            <p class="text-gray-800 text-xs px-2 mt-2">
+            Add additional file factories, for instance front end frameworks.
+            </p>                                              
+        </div>
+
     </div>  
 </template>
 
