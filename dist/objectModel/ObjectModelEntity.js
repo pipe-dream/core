8499ffa79c65ffc8cfac58474d7b18b3f2d7f8a8
Anders Jurisoo <jurisoo@hotmail.com>
2019-09-05 09:27:29 +0200

    Rebuild

--- dist/objectModel/ObjectModelEntity.js
+++ dist/objectModel/ObjectModelEntity.js
@@ -97,7 +97,7 @@ var ObjectModelEntity = /** @class */ (function () {
             type: this.constructor.name,
             //attributes: this.attributes.map(attribute => attribute.serialize()),
             attributes: this.attributes.reduce(function (carry, attribute) {
-                carry[attribute.name] = attribute.serialize();
+                carry[attribute.properties.name] = attribute.serialize();
                 return carry;
             }, {}),
             relationships: {
