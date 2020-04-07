fa8eca57ae6363d74863144b8fa6332c184887ae
septio <godofdenmark@gmail.com>
2020-04-07 13:41:50 +0200

    Changed how exports are done

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,40 @@
-import './css/app.css';
-require('./utilities/extendArray.js');
-import Vue from 'vue';
+import './css/app.css'
+
+require('./utilities/extendArray.js')
+import {VueConstructor} from 'vue'
 import VTooltip from 'v-tooltip'
 
 export const PipeDreamVueTools = {
-    install: function (Vue: Vue.VueConstructor, options: {}) {
+    install: function (Vue: VueConstructor, options: Record<string, any> = {}): void {
         /* Register all vue components */
-        //@ts-ignore
-        const files = require.context('./', true, /\.vue$/i);
-        files.keys().map((key: string) => Vue.component(
-            //@ts-ignore
-            key.split('/').pop().split('.')[0],
-            files(key).default)
-        );
-       Vue.use(VTooltip)
+
+        const files = require.context('./', true, /\.vue$/i)
+        files.keys().map((key: string) =>
+            Vue.component(
+                key
+                    .split('/')
+                    .pop()
+                    .split('.')[0],
+                files(key).default
+            )
+        )
+        Vue.use(VTooltip)
     }
 }
 
 export const Globals = {
-    SINGLE_LINE_BREAK: "\n",
-    get DOUBLE_LINE_BREAK() {
+    SINGLE_LINE_BREAK: '\n',
+    get DOUBLE_LINE_BREAK(): string {
         return this.SINGLE_LINE_BREAK.repeat(2)
     }
 }
 
-export const PipeDream = require("./PipeDream").PipeDream;
-export const defaultStore = require("./defaultStore");
-export const Template = require("./utilities/Template").Template;
-export const SketchButton = require("./utilities/SketchButton").SketchButton
-export const BaseFileFactory = require("./fileFactories/BaseFileFactory").BaseFileFactory
-export const ModelEntity = require("./objectModel/ObjectModelEntity").ObjectModelEntity
-export const Formatter = require("./utilities/Formatter").Formatter;
-export const Schema = require("./objectModel/Schema").Schema
-export const Pipe = require("./pipes/Pipe").Pipe;
+export {PipeDream} from './PipeDream'
+export {DefaultStore} from './defaultStore'
+export {Template} from './utilities/Template'
+export {SketchButton} from './utilities/SketchButton'
+export {BaseFileFactory} from './fileFactories/BaseFileFactory'
+export {ObjectModelEntity} from './objectModel/ObjectModelEntity'
+export {Formatter} from './utilities/Formatter'
+export {Schema} from './objectModel/Schema'
+export {Pipe} from './pipes/Pipe'
